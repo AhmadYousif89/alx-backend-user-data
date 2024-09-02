2ead5f435adb3f1aace97b0d600a1a759116f198
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-02 19:09:08 +0300

    Update error handler return types

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -43,24 +43,24 @@ def auth_checkpoint() -> None:
 
 
 @app.errorhandler(404)
-def not_found(error) -> tuple:
+def not_found(error) -> str:
     """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> tuple:
+def unauthorized(error) -> str:
     """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> tuple:
+def forbidden(error) -> str:
     """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = int(getenv("API_PORT", "5000"))
+    port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
