9c4faef54a1032b600426e28fc996a726f06581d
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-04 00:56:07 +0300

    Refactor authentication module

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,46 +2,38 @@
 """ Authentication module for the API
 """
 
-from flask import request
 from typing import List, TypeVar
 from models.user import User
 
 
-USR = TypeVar('User')
+class Auth:
+    """Authentication class"""
 
-
-class Auth():
-    """ Authentication class
-    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Checks if the given request path is not in the list of
+        """Checks if the given request path is not in the list of
         excluded paths and returns False if it is, and True if it is not.
         """
-        if path is None:
+        if not path or not excluded_paths:
             return True
-        if excluded_paths is None or not excluded_paths:
-            return True
-
-        s_path = path.rstrip('/')
+        normalized_path = path.rstrip('/')
 
         for excluded_path in excluded_paths:
-            s_excluded_path = excluded_path.rstrip('/')
-            if s_path == s_excluded_path:
+            normalized_excluded_path = excluded_path.rstrip('/')
+            if normalized_excluded_path.endswith('*'):
+                if normalized_path.startswith(normalized_excluded_path[:-1]):
+                    return False
+            elif normalized_path == normalized_excluded_path:
                 return False
 
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Returns the value of the Authorization header from a request
-        """
-        if request is None:
-            return None
-        if request.headers.get('Authorization') is None:
+        """Returns the value of the Authorization header from a request"""
+        if request is None or request.headers.get('Authorization') is None:
             return None
 
         return request.headers.get('Authorization')
 
-    def current_user(self, request=None) -> USR:
-        """ public method
-        """
+    def current_user(self, request=None) -> TypeVar('User'):
+        """public method"""
         return None
