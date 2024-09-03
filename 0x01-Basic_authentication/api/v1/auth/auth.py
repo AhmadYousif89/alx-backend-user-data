9a45fa960ed55e9d1cc4bc3c540e41af4701bcc7
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-03 15:09:26 +0300

    Refactor authentication logic to allowing * at the end of excluded paths.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,8 +18,12 @@ class Auth:
             return True
         if path[-1] != '/':
             path += '/'  # add trailing slash
-        if path in excluded_paths:
-            return False  # do not require authentication
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
         return True  # require authentication
 
     def authorization_header(self, request=None) -> str:
