0f5c7c915988ac6ba19705d69bb2b675cb6c60b8
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-04 23:30:31 +0300

    Fix user search error in session authentication view

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -27,14 +27,16 @@ def auth_session_login():
     password = request.form.get('password')
     if not password:
         return jsonify({"error": "password missing"}), 400
-
-    user = User.search({'email': email})[0]
+    try:
+        user = User.search({'email': email})[0]
+    except IndexError:
+        return jsonify({"error": "no user found for this email"}), 404
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
-    from api.v1.auth import auth
+    from api.v1.app import auth  # Global auth object
 
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
