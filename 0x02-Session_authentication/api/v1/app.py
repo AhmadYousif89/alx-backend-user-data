7ccf0328b88845726007649420e3e4da167e45ee
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-04 20:32:10 +0300

    Fix authentication issue in app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,7 +39,10 @@ def auth_checkpoint() -> None:
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/',
     ]
+    if auth.authorization_header(request) and auth.session_cookie(request):
+        abort(401)
     if auth.require_auth(request.path, excluded_paths):
         if not auth.authorization_header(request):
             abort(401)
