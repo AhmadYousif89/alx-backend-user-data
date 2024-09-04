f9b71f901ee7be8ea377fb42dc1b2f19be001a06
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-04 23:51:05 +0300

    Add session expiration authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -29,6 +29,11 @@ if AUTH_TYPE == "session_auth":
 
     auth = SessionAuth()
 
+if AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+
+    auth = SessionExpAuth()
+
 
 @app.before_request
 def auth_checkpoint() -> None:
