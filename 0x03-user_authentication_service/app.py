2265ee5c6d80d95a4582413547790f1c6ad3f029
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-11 22:56:37 +0300

    Add reset password endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,5 +97,19 @@ def logout():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """POST /reset_password
+    Return:
+        - Response object and a status code
+    """
+    email = request.form.get("email") or ""
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token})
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
