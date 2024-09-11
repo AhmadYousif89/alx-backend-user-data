5a57a527d0766522b6670ddcfdff5befb0a6b1ae
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-11 17:26:49 +0300

    Add user registration functionality to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,8 +4,29 @@ Auth module
 """
 
 import bcrypt
+from db import DB, User
 
 
 def _hash_password(password: str) -> bytes:
     """Takes in a password string arguments and returns bytes"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user"""
+        from sqlalchemy.orm.exc import NoResultFound  # type: ignore
+
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password.decode())
+        raise RuntimeError("Unexpected condition in register_user")
