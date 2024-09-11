9e87a642fcba1ccb107b51c173cb3a0adbdfddd2
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-11 22:05:03 +0300

    Add get_users endpoint and logout functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Simple Flask app
 """
 
-from flask import Flask, abort, jsonify, request
+from flask import Flask, abort, jsonify, request, redirect
 from auth import Auth
 
 
@@ -19,6 +19,16 @@ def root():
       - message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['Get'])
+def get_users():
+    """GET /users
+    Return:
+        - Response object and a status code
+    """
+    users = AUTH.get_users()
+    return jsonify([user.to_json() for user in users])
 
 
 @app.route('/users', methods=['POST'])
@@ -42,15 +52,36 @@ def login():
     Return:
         - Response object and a status code
     """
+    if not request.form.get("email"):
+        return jsonify({"message": "email is missing"}), 400
+    if not request.form.get("password"):
+        return jsonify({"message": "password is missing"}), 400
+
     email = request.form['email']
     password = request.form['password']
     if AUTH.valid_login(email, password):
-        session_id = AUTH.create_session(email)
+        session_id = AUTH.create_session(email) or ""
         response = jsonify({"email": email, "message": "logged in"})
         response.set_cookie("session_id", session_id)
         return response
     else:
         abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """DELETE /sessions
+    Return:
+        - Response object and a status code
+    """
+    session_id = request.cookies.get("session_id") or ""
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        assert isinstance(user.id, int), "user.id must be an integer"
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
