e342ce70564186d3b0c6af4f8a5c7ce4363bf4ef
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-11 18:20:39 +0300

    Update login route to handle invalid credentials

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Simple Flask app
 """
 
-from flask import Flask, Response, jsonify, request
+from flask import Flask, abort, jsonify, request
 from auth import Auth
 
 
@@ -34,6 +34,23 @@ def register_user() -> str:
         return jsonify({"email": user.email, "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """POST /sessions
+    Return:
+        - Response object and a status code
+    """
+    email = request.form['email']
+    password = request.form['password']
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
